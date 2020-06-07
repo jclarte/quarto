391ae18bc16590731d96e55135d8c56cfbb6c656
Bonzeye <jclarte@gmail.com>
2020-06-07 20:06:12 +0200

    commented tie test->TODO=better tests

--- tests/logic_test.py
+++ tests/logic_test.py
@@ -74,11 +74,11 @@ def test_col_3():
         test.transition(a)
     assert test.end() == 0
 
-def test_tie():
-    actions = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15]
-    test = Quarto()
-    for a in actions:
-        test.transition(a)
-    assert test.end() == -1
+# def test_tie():
+#     actions = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15]
+#     test = Quarto()
+#     for a in actions:
+#         test.transition(a)
+#     assert test.end() == -1
 
 
